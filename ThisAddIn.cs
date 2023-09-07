78d327590742e8503d865e3fc1f9cc3a1732ebc5
Abdullah Omar <ashwaliab@outlook.com>
2023-09-07 10:12:29 +0300

    VSTO test

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -8,7 +8,19 @@ using IronXL;
 
 
 class ThisAddIn
-{
-    
+ public partial class test
+    {
+        private void test_load(object sender, RibbonUIEventArgs e)
+        {
 
-}
+        }
+        //this method is to create a button and try to display the button
+        private void testButton_clicl(object sender, RibbonControlEventArgs e)
+        {
+            // This executed when the button is clicked
+            Excel.Application excelApp = Globals.ThisAddIn.Application;
+            
+            // Perform display a message box
+            Excel.Worksheet activeSheet = excelApp.ActiveSheet as Excel.Worksheet;
+        }
+    }
